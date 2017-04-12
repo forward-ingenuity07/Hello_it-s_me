26a52edf8ce634beba631f6112bbf8066abf7a87
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-13 01:41:19 +0200

    5th commit

--- Hello_its_me/www/scripts/index.js
+++ Hello_its_me/www/scripts/index.js
@@ -15,14 +15,20 @@
         document.getElementById("ButtPress").onclick = function () {
             
             var onSuccess = function (position) {
-                alert('Latitude: ' + position.coords.latitude + '\n' +
+                var posLat = -25.7503767;
+                if (position.coords.latitude == posLat)
+                {
+                    alert('You have arrived');
+
+                }
+                /*alert('Latitude: ' + position.coords.latitude + '\n' +
                       'Longitude: ' + position.coords.longitude + '\n' +
                       'Altitude: ' + position.coords.altitude + '\n' +
                       'Accuracy: ' + position.coords.accuracy + '\n' +
                       'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
                       'Heading: ' + position.coords.heading + '\n' +
                       'Speed: ' + position.coords.speed + '\n' +
-                      'Timestamp: ' + position.timestamp + '\n');
+                      'Timestamp: ' + position.timestamp + '\n');*/
             };
 
             // onError Callback receives a PositionError object
@@ -44,6 +50,8 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        
+
     };
 
     function onPause() {
