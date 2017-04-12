f0a7f86720e63ead67081b1a80afc8655ad0d13f
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-13 00:59:05 +0200

    Fourth commit

--- Hello_its_me/www/scripts/index.js
+++ Hello_its_me/www/scripts/index.js
@@ -13,7 +13,30 @@
         document.addEventListener('resume', onResume.bind(this), false);
 
         document.getElementById("ButtPress").onclick = function () {
-            alert("Good day Sir")
+            
+            var onSuccess = function (position) {
+                alert('Latitude: ' + position.coords.latitude + '\n' +
+                      'Longitude: ' + position.coords.longitude + '\n' +
+                      'Altitude: ' + position.coords.altitude + '\n' +
+                      'Accuracy: ' + position.coords.accuracy + '\n' +
+                      'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                      'Heading: ' + position.coords.heading + '\n' +
+                      'Speed: ' + position.coords.speed + '\n' +
+                      'Timestamp: ' + position.timestamp + '\n');
+            };
+
+            // onError Callback receives a PositionError object
+            //
+            function onError(error) {
+                alert('code: ' + error.code + '\n' +
+                      'message: ' + error.message + '\n');
+            }
+
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
+
+
         };
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
